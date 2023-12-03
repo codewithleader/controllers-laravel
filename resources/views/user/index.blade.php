7b5c1210603363615016a7087943d3649afe76f4
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-12-03 16:31:08 -0500

    @forelse

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>Home</title>
 </head>
+
 <body>
   <h1>User List:</h1>
-  {{-- Directiva Selectiva if() --}}
-  @if ($users->isEmpty())
-      <p>The user list is empty</p>
-  @else
   <ul>
     {{-- Directiva Interactiva @foreach --}}
-    @foreach ($users as $user)
-    <li>
-      <h3>{{ $user->name }}</h3>
-      <p>{{ $user->email }}</p>
-    </li>
-    @endforeach
+    @forelse($users as $user)
+      <li>
+        <h3>{{ $user->name }}</h3>
+        <p>{{ $user->email }}</p>
+      </li>
+    @empty
+      <li>List empty.</li>
+    @endforelse
   </ul>
-  @endif
 </body>
+
 </html>
